eea2bf52d510cca3c8ab53fe96e7e1d9f74f4604
JoseLuis <vazquezjluis@yahoo.com>
2019-05-27 02:46:16 -0300

    primer commit

---
+++ app-old.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-type': 'application/json' });
+
+        let salida = {
+            nombre: 'Jose',
+            edad: 32,
+            url: req.url
+        }
+
+        res.write(JSON.stringify(salida));
+        res.end();
+    })
+    .listen(8080);
+
+
+console.log('Escuchando el puerto 8080');
